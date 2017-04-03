479b8cc696aa4dfb1e7abac7666dca597fa4b9e8
turlir <iljaosincev@gmail.com>
2017-04-03 18:55:16 +0500

    move database inserting to side-effect code block

--- app/src/main/java/com/turlir/abakgists/AllGistsPresenter.java
+++ app/src/main/java/com/turlir/abakgists/AllGistsPresenter.java
@@ -2,11 +2,18 @@ package com.turlir.abakgists;
 
 
 import com.pushtorefresh.storio.sqlite.StorIOSQLite;
+import com.pushtorefresh.storio.sqlite.operations.put.PutResults;
 import com.turlir.abakgists.base.BasePresenter;
 import com.turlir.abakgists.model.Gist;
 import com.turlir.abakgists.network.ApiClient;
 
 import java.util.List;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class AllGistsPresenter extends BasePresenter<AllGistsFragment> {
 
@@ -24,16 +31,24 @@ public class AllGistsPresenter extends BasePresenter<AllGistsFragment> {
         currentSize = Math.max(currentSize, PAGE_SIZE);
         int page = currentSize / PAGE_SIZE;
         addSubscription(mClient.publicGist(page)
-                .compose(this.<List<Gist>>defaultSchedule())
+                .doOnNext(new Action1<List<Gist>>() {
+                    @Override
+                    public void call(List<Gist> gists) {
+                        if (gists != null && !gists.isEmpty()) {
+                            mDatabase.put()
+                                    .objects(gists)
+                                    .prepare()
+                                    .executeAsBlocking();
+                        }
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Handler<List<Gist>>() {
 
                     @Override
                     public void onNext(List<Gist> value) {
                         if (getView() != null) {
-                            mDatabase.put()
-                                    .objects(value)
-                                    .prepare()
-                                    .executeAsBlocking();
                             if (value.size() < PAGE_SIZE) {
                                 // достигнут конец списка
                                 getView().stopGistLoad();
