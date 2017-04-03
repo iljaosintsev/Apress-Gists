1fdfdcbc428f07a8023d635d7d682056ff42846d
turlir <iljaosincev@gmail.com>
2017-04-04 00:00:34 +0500

    up class logger for database

--- app/src/main/java/com/turlir/abakgists/di/DatabaseModule.java
+++ app/src/main/java/com/turlir/abakgists/di/DatabaseModule.java
@@ -3,16 +3,13 @@ package com.turlir.abakgists.di;
 import android.content.Context;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.NonNull;
-import android.util.Log;
 
 import com.pushtorefresh.storio.sqlite.SQLiteTypeMapping;
 import com.pushtorefresh.storio.sqlite.StorIOSQLite;
 import com.pushtorefresh.storio.sqlite.impl.DefaultStorIOSQLite;
-import com.pushtorefresh.storio.sqlite.operations.put.PutResult;
 import com.turlir.abakgists.model.Gist;
 import com.turlir.abakgists.model.GistStorIOSQLiteDeleteResolver;
 import com.turlir.abakgists.model.GistStorIOSQLiteGetResolver;
-import com.turlir.abakgists.model.GistStorIOSQLitePutResolver;
 
 import javax.inject.Singleton;
 
@@ -22,22 +19,6 @@ import dagger.Provides;
 
 @Module
 public class DatabaseModule {
-
-    private final GistStorIOSQLitePutResolver mLoggerResolver = new GistStorIOSQLitePutResolver() {
-        @NonNull
-        @Override
-        public PutResult performPut(@NonNull StorIOSQLite storIOSQLite, @NonNull Gist object) {
-            PutResult result = super.performPut(storIOSQLite, object);
-
-            if (result.wasInserted()) {
-                Long iid = result.insertedId();
-                if (iid != null && iid != -1) {
-                    Log.d("DATABASE", object.id + " inserted");
-                }
-            }
-            return result;
-        }
-    };
 
     @Provides
     @Singleton
@@ -49,7 +30,7 @@ public class DatabaseModule {
     @Singleton
     public StorIOSQLite provideStorIOSQLite(@NonNull SQLiteOpenHelper sqLiteOpenHelper) {
         SQLiteTypeMapping<Gist> typeMapping = SQLiteTypeMapping.<Gist>builder()
-                .putResolver(mLoggerResolver)
+                .putResolver(new GistStorIoLogPutResolver()) // logger
                 .getResolver(new GistStorIOSQLiteGetResolver())
                 .deleteResolver(new GistStorIOSQLiteDeleteResolver())
                 .build();
