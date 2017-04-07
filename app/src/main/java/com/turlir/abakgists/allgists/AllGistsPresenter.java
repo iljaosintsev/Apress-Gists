1856685419dbec1bfac5226c7df816585891dbaa
turlir <iljaosincev@gmail.com>
2017-04-08 02:04:40 +0500

    split download data from server and use cache repository support plus one method loadGistsFromServerAndPutCache

--- app/src/main/java/com/turlir/abakgists/allgists/AllGistsPresenter.java
+++ app/src/main/java/com/turlir/abakgists/allgists/AllGistsPresenter.java
@@ -3,6 +3,7 @@ package com.turlir.abakgists.allgists;
 
 import android.util.Log;
 
+import com.pushtorefresh.storio.sqlite.operations.put.PutResults;
 import com.turlir.abakgists.base.BasePresenter;
 import com.turlir.abakgists.model.Gist;
 import com.turlir.abakgists.network.Repository;
@@ -19,36 +20,66 @@ import rx.schedulers.Schedulers;
 public class AllGistsPresenter extends BasePresenter<AllGistsFragment> {
 
     private final Repository mRepo;
+    private Subscription mCacheSubs;
 
     public AllGistsPresenter(Repository repo) {
         mRepo = repo;
     }
 
     void loadPublicGists(final int currentSize) {
-        Observable<List<Gist>> source = mRepo.loadGists(currentSize);
-        Subscription subs = source
-                .compose(this.<List<Gist>>defaultScheduler())
+        if (mCacheSubs != null) {
+            if (!mCacheSubs.isUnsubscribed()) {
+                removeSubscription(mCacheSubs);
+            }
+        }
+        mCacheSubs = mRepo.loadGistsFromCache(currentSize)
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Handler<List<Gist>>() {
                     @Override
                     public void onNext(List<Gist> value) {
                         Log.d("AllGistPresenter", "onNext " + value.size());
-                        if (getView() != null && value.size() > 0) {
-                            getView().onGistLoaded(value, currentSize, value.size());
+                        if (getView() != null) {
+                            if (value.size() > 0) {
+                                getView().onGistLoaded(value, currentSize, value.size());
+                            } else {
+                                loadFromServer(currentSize);
+                            }
                         }
                     }
                 });
+        addSubscription(mCacheSubs);
+    }
 
-        addSubscription(subs);
+    private void loadFromServer(int currentSize) {
+        Subscription subsToServer = mRepo.loadGistsFromServerAndPutCache(currentSize)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<PutResults<Gist>>() {
+                    @Override
+                    public void call(PutResults<Gist> gistPutResults) {
+                        Log.d("AllGistsPresenter", "fromServer obs " + gistPutResults.results().size());
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        processError(throwable);
+                    }
+                });
+        addSubscription(subsToServer);
     }
 
     void resetGist() {
+        if (mCacheSubs != null) {
+            if (!mCacheSubs.isUnsubscribed()) {
+                removeSubscription(mCacheSubs);
+            }
+        }
         Subscription subs = mRepo.clearCache()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action0() {
                     @Override
                     public void call() {
-
+                        loadPublicGists(0);
                     }
                 }, new Action1<Throwable>() {
                     @Override
--- app/src/main/java/com/turlir/abakgists/network/Repository.java
+++ app/src/main/java/com/turlir/abakgists/network/Repository.java
@@ -1,7 +1,5 @@
 package com.turlir.abakgists.network;
 
-import android.util.Log;
-
 import com.pushtorefresh.storio.sqlite.StorIOSQLite;
 import com.pushtorefresh.storio.sqlite.operations.put.PutResults;
 import com.pushtorefresh.storio.sqlite.queries.DeleteQuery;
@@ -14,51 +12,26 @@ import java.util.List;
 import rx.Completable;
 import rx.Observable;
 import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
 
 public class Repository {
 
-    private static final String TAG = "REPO";
     private static final int PAGE_SIZE = 30;
 
     private ApiClient mClient;
     private StorIOSQLite mDatabase;
 
     public Repository(ApiClient client, StorIOSQLite base) {
-        this.mClient = client;
-        this.mDatabase = base;
+        mClient = client;
+        mDatabase = base;
     }
 
-    public Observable<List<Gist>> loadGists(int currentSize) {
-        final int page = currentSize / PAGE_SIZE + 1;
-        Observable<List<Gist>> cacheData = createCacheObservable(currentSize);
-        return cacheData
-                .flatMap(new Func1<List<Gist>, Observable<List<Gist>>>() {
-                    @Override
-                    public Observable<List<Gist>> call(List<Gist> gists) {
-                        Log.d(TAG, "fromCache " + gists.size());
-                        if (gists.size() < 1) {
-                            return createServerObservable(page);
-                        }
-                        return Observable.just(gists);
-                    }
-                });
-    }
-
-    private Observable<List<Gist>> createCacheObservable(int currentSize) {
-       return mDatabase.get()
-                .listOfObjects(Gist.class)
-                .withQuery(
-                        Query.builder()
-                                .table(GistsTable.GISTS)
-                                .limit(currentSize, PAGE_SIZE)
-                                .build()
-                )
-                .prepare()
-                .asRxObservable(); // подписка на обновления
-    }
-
-    private Observable<List<Gist>> createServerObservable(int page) {
-        return mClient.publicGist(page)
+    public Observable<PutResults<Gist>> loadGistsFromServerAndPutCache(int currentSize) {
+        int page = currentSize / PAGE_SIZE + 1;
+        return mClient
+                .publicGist(page)
+                .subscribeOn(Schedulers.io())
                 .map(new Func1<List<Gist>, List<Gist>>() {
                     @Override
                     public List<Gist> call(List<Gist> gists) {
@@ -74,21 +47,25 @@ public class Repository {
                 .flatMap(new Func1<List<Gist>, Observable<PutResults<Gist>>>() {
                     @Override
                     public Observable<PutResults<Gist>> call(List<Gist> gists) {
-                        Log.d("REPO", "fromServerToCache " + gists.size());
                         return mDatabase.put()
                                 .objects(gists)
                                 .prepare()
                                 .asRxObservable();
                     }
-                })
-                .skip(1)
-                .concatMapIterable(new Func1<PutResults<Gist>, Iterable<Gist>>() {
-                    @Override
-                    public Iterable<Gist> call(PutResults<Gist> gistPutResults) {
-                        return gistPutResults.results().keySet();
-                    }
-                })
-                .toList();
+                });
+    }
+
+    public Observable<List<Gist>> loadGistsFromCache(int currentSize) {
+        return mDatabase.get()
+                .listOfObjects(Gist.class)
+                .withQuery(
+                        Query.builder()
+                                .table(GistsTable.GISTS)
+                                .limit(currentSize, PAGE_SIZE)
+                                .build()
+                )
+                .prepare()
+                .asRxObservable();
     }
 
     public Completable clearCache() {
@@ -102,4 +79,5 @@ public class Repository {
                 .prepare()
                 .asRxCompletable();
     }
+
 }
