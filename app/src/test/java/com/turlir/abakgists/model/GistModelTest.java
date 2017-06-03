64f565a08caad8449c12accc9f847e3e8135456f
iljaosintsev <iljaosincev@gmail.com>
2017-06-04 00:07:06 +0500

    split GistModelTest

--- app/src/test/java/com/turlir/abakgists/model/GistModelTest.java
+++ app/src/test/java/com/turlir/abakgists/model/GistModelTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class GistModelTest {
 
     @Test
-    public void testEquals() {
+    public void testEqualsTrue() {
         GistModel one = new GistModel();
         one.url = "url";
         one.note = null;
@@ -25,12 +25,31 @@ public class GistModelTest {
         two.ownerAvatarUrl = null;
         two.ownerLogin = "login";
 
-        boolean equals = two.equals(one);
-        org.junit.Assert.assertTrue(equals);
-
-        two.note = "note";
-        equals = two.equals(one);
-        org.junit.Assert.assertFalse(equals);
+        org.junit.Assert.assertEquals(two, one);
     }
+
+    @Test
+    public void testEqualsDifferentNote() {
+        GistModel one = new GistModel();
+        one.url = "url";
+        one.note = null;
+        one.description = null;
+        one.created = "created";
+        one.id = "id";
+        one.ownerAvatarUrl = null;
+        one.ownerLogin = "login";
+
+        GistModel two = new GistModel();
+        two.url = "url";
+        two.note = "note"; // alarm
+        two.description = null;
+        two.created = "created";
+        two.id = "id";
+        two.ownerAvatarUrl = null;
+        two.ownerLogin = "login";
+
+        org.junit.Assert.assertNotEquals(two, one);
+    }
+
 
 }
