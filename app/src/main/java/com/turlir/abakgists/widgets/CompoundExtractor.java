cba12b1123a954bb4bbd8718735d3babfc22e07f
iljaosintsev <iljaosincev@gmail.com>
2017-06-17 22:02:38 +0500

    CompoundExtractor improvement
    - support AnimatedVectorDrawableCompat

--- app/src/main/java/com/turlir/abakgists/widgets/CompoundExtractor.java
+++ app/src/main/java/com/turlir/abakgists/widgets/CompoundExtractor.java
@@ -3,6 +3,7 @@ package com.turlir.abakgists.widgets;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.graphics.drawable.VectorDrawableCompat;
 import android.util.AttributeSet;
 
@@ -10,8 +11,21 @@ import com.turlir.abakgists.R;
 
 abstract class CompoundExtractor {
 
-    static Drawable[] extract(Context context, AttributeSet attrs, Drawable[] pd) {
+    private static final int[] IDS = new int[] {
+            R.styleable.VectorButton_drawableLeftCompat,
+            R.styleable.VectorButton_animateDrawableLeftCompat,
 
+            R.styleable.VectorButton_drawableTopCompat,
+            R.styleable.VectorButton_animateDrawableTopCompat,
+
+            R.styleable.VectorButton_drawableRightCompat,
+            R.styleable.VectorButton_animateDrawableRightCompat,
+
+            R.styleable.VectorButton_drawableBottomCompat,
+            R.styleable.VectorButton_animateDrawableBottomCompat
+    };
+
+    static Drawable[] extract(Context context, AttributeSet attrs, Drawable[] pd) {
         for (Drawable drawable : pd) {
             if (drawable != null) {
                 throw new UnsupportedOperationException("do not use sdk compound drawable attributes");
@@ -19,27 +33,31 @@ abstract class CompoundExtractor {
         }
 
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.VectorButton);
+        Drawable[] dr = new Drawable[4];
         try {
-
-            Drawable leftDrawable = getDrawable(context, ta, R.styleable.VectorButton_drawableLeftCompat);
-            Drawable rightDrawable = getDrawable(context, ta, R.styleable.VectorButton_drawableRightCompat);
-            Drawable topDrawable = getDrawable(context, ta, R.styleable.VectorButton_drawableTopCompat);
-            Drawable bottomDrawable = getDrawable(context, ta, R.styleable.VectorButton_drawableBottomCompat);
-
-            return new Drawable[] {
-                    leftDrawable, topDrawable, rightDrawable, bottomDrawable
-            };
-
+            for (int i = 0; i < IDS.length; i+=2) {
+                int f = IDS[i];
+                int s = IDS[i + 1];
+                dr[i / 2] = getDrawable(context, ta, f, s);
+            }
+            return dr;
         } finally {
             ta.recycle();
         }
     }
 
-    private static Drawable getDrawable(Context context, TypedArray ta, int id) {
-        int leftId = ta.getResourceId(id, -1);
-        if (leftId != -1) {
-            return VectorDrawableCompat.create(context.getResources(), leftId, context.getTheme());
+    private static Drawable getDrawable(Context context, TypedArray ta, int std, int animate) {
+        int stdRes = ta.getResourceId(std, -1);
+        int animRes = ta.getResourceId(animate, -1);
+        if (stdRes != -1 && animRes != -1) {
+            throw new IllegalStateException("drawableCompat and animateDrawableCompat attr at the same time");
         }
-        return null;
+        if (stdRes != -1) {
+            return VectorDrawableCompat.create(context.getResources(), stdRes, context.getTheme());
+        } else if(animRes != -1) {
+            return AnimatedVectorDrawableCompat.create(context, animRes);
+        } else {
+            return null;
+        }
     }
 }
