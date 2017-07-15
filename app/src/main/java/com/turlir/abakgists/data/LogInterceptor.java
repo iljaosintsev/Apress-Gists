4e87f333c91fb09ab1fcf59290bf04a2367684cf
iljaosintsev <iljaosincev@gmail.com>
2017-07-16 01:41:09 +0500

    improve LogInterceptor
    - Timber.i optimize
    
    - pretty json response logging

--- app/src/main/java/com/turlir/abakgists/data/LogInterceptor.java
+++ app/src/main/java/com/turlir/abakgists/data/LogInterceptor.java
@@ -1,5 +1,10 @@
 package com.turlir.abakgists.data;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
 
 import okhttp3.Interceptor;
@@ -7,45 +12,44 @@ import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
-import okio.Buffer;
 import timber.log.Timber;
 
 public class LogInterceptor implements Interceptor {
 
+    private final Gson mGson;
+    private final JsonParser mParser;
+
+    public LogInterceptor() {
+        mGson = new GsonBuilder().setPrettyPrinting().create();
+        mParser = new JsonParser();
+    }
+
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
-        log("Request: " + request.url().toString());
+        Timber.i("Request: %s", request.url().toString());
         if (request.method().equals("POST")) {
-            log("Request body: " + body2str(request));
+            Timber.i("Request body: %s", request);
         }
 
         Response response = chain.proceed(request);
         String bodyString = response.body().string();
-        log("Response (" + response.code() + "): " + bodyString);
+        if ("application/json; charset=utf-8".equals(response.headers().get("Content-Type"))) {
+            Timber.i("Response (%d)", response.code());
+            prettyJsonLog(bodyString);
+        } else {
+            Timber.i("Response (%d): %s", response.code(), bodyString);
+        }
 
         MediaType mediaType = response.body().contentType();
         ResponseBody body = ResponseBody.create(mediaType, bodyString);
         return response.newBuilder().body(body).build();
     }
 
-    private String body2str(final Request request) {
-        try {
-            final Request copy = request.newBuilder().build();
-            final Buffer buffer = new Buffer();
-            if ((copy.body().contentLength() > 1024 * 1024)) { // > 1 MB
-                return "<too long>";
-            } else {
-                copy.body().writeTo(buffer);
-                return buffer.readUtf8();
-            }
-        } catch (IOException e) {
-            return "<no body>";
-        }
-    }
-
-    private static void log(String message) {
-        Timber.i(message);
+    private void prettyJsonLog(String message) {
+        JsonElement json = mParser.parse(message);
+        String prettyJson = mGson.toJson(json);
+        Timber.i(prettyJson);
     }
 
 }
