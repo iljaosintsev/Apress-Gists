e8c7494fe8706d4a548db9b4bd410b7ad03b0324
iljaosintsev <iljaosincev@gmail.com>
2018-08-31 16:28:54 +0500

    simplify Params, reverse animation

--- app/src/main/java/com/turlir/abakgists/MainActivity.java
+++ app/src/main/java/com/turlir/abakgists/MainActivity.java
@@ -3,12 +3,17 @@ package com.turlir.abakgists;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
+import android.animation.TimeInterpolator;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 
 import com.turlir.abakgists.allgists.view.AllGistsActivity;
 import com.turlir.abakgists.notes.view.NotesActivity;
@@ -34,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.btn_all_in_one)
     View btnAll;
 
-    private Pair<Params, Params> animateParams;
     private AnimatorSet animation;
 
     @Override
@@ -47,9 +51,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if (animateParams != null) {
-            AnimatorSet set = animateButton(animateParams.second);
-            set.start();
+        if (animation != null) {
+            compatReverse(animation);
         }
     }
 
@@ -78,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void buttonClick(View clicked, final Class clazz) {
-        animateParams = new Params.TwoWayBuilder(clicked)
+        Pair<Params, Params> animateParams = new Params.TwoWayBuilder(clicked)
                 .up(btnAllGists)
                 .center(btnNotes)
                 .down(btnAll)
@@ -100,5 +103,44 @@ public class MainActivity extends AppCompatActivity {
         return factory.animate(btnAllGists, btnNotes, btnAll);
     }
 
+    private void compatReverse(AnimatorSet directAnim) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            oreoReverse(directAnim);
+        } else {
+            oldReverse(directAnim);
+        }
+    }
 
+    @TargetApi(Build.VERSION_CODES.O)
+    private void oreoReverse(AnimatorSet directAnim) {
+        if (directAnim.getTotalDuration() != Animator.DURATION_INFINITE) {
+            directAnim.reverse();
+        }
+    }
+
+    private void oldReverse(AnimatorSet directAnim) {
+
+        class ReverseInterpolator implements Interpolator {
+
+            private final TimeInterpolator delegate;
+
+            private ReverseInterpolator(TimeInterpolator delegate){
+                this.delegate = delegate;
+            }
+
+            private ReverseInterpolator(){
+                this(new LinearInterpolator());
+            }
+
+            @Override
+            public float getInterpolation(float input) {
+                return 1 - delegate.getInterpolation(input);
+            }
+        }
+
+        AnimatorSet back = new AnimatorSet();
+        back.play(directAnim);
+        back.setInterpolator(new ReverseInterpolator());
+        back.start();
+    }
 }
--- app/src/main/java/com/turlir/abakgists/widgets/anim/factor/Params.java
+++ app/src/main/java/com/turlir/abakgists/widgets/anim/factor/Params.java
@@ -45,8 +45,8 @@ public class Params {
         return distance.get(position);
     }
 
-    private Params distance(int multi, float add, float value) {
-        distance.add(value * multi + add);
+    private Params distance(int multi, int add, float value) {
+        distance.add(multi * (add + value));
         return this;
     }
 
@@ -126,26 +126,18 @@ public class Params {
             Params there = new Params(),
                     back = new Params();
 
+            int center = (mViews[2].getTop() - mViews[0].getTop()) / 2;
+
             for (View item : mViews) {
                 there.alpha(mSelected == item ? 1 : 0f);
                 back.alpha(1f);
-
                 if (item == mSelected) {
-                    float distance = Math.abs(mSelected.getY() - mViews[1].getY()) - item.getHeight();
-                    int sign = Float.compare(mViews[1].getY(), mSelected.getY());
-                    float tadd = distance * sign;
-                    int tm = Float.compare(mViews[1].getY(), item.getY());
-                    there.distance(tm, tadd, item.getHeight());
-
-                    int bm = Float.compare(item.getY(), mViews[1].getY()) / 2;
-                    back.distance(bm, 0f, item.getHeight());
-
+                    there.distance(Integer.compare(center, -item.getTop()), center, -item.getTop());
+                    back.distance(0, 0, item.getHeight());
                 } else {
                     int tm = Float.compare(item.getY(), mSelected.getY());
-                    there.distance(tm, 0f, item.getHeight());
-
-                    int bm = Float.compare(mSelected.getY(), mViews[1].getY()) / 2;
-                    back.distance(bm, 0f, item.getHeight());
+                    there.distance(tm, 0, item.getHeight());
+                    back.distance(0, 0, item.getHeight());
                 }
             }
 
@@ -157,87 +149,4 @@ public class Params {
             return new Pair<>(there, back);
         }
     }
-
-    /**
-     * Создает один экземпляр класса Params, описывающий анимацию трех view в одном направлении. <p>
-     * Конструктор принимает ссылку на якорную view, методы {@link #up}, {@link #center} и {@link #down} задают
-     * местоположение view друг относительно друга. <p>
-     * Параметры анимации рассчитываются с учетом положения view друг относительно друга.
-     * Прозрачность якорной view приравнивается к 1, остальных – к 0. Смещение якорной view равно
-     * расстоянию от нее до центральной view. Остальные view перемещаются на величину свой высоты в противоположную
-     * от якорной view сторону. Таким образом якорная view не пересекается с периферийными.
-     */
-    public static class Builder {
-
-        private View mSelected;
-        private View[] mViews;
-
-        /**
-         * @param selected якорная view
-         */
-        public Builder(View selected) {
-            mSelected = selected;
-            mViews = new View[3];
-        }
-
-        /**
-         * @param v view, которая находится выше остальных
-         * @return Builder fluent-interface
-         */
-        public Builder up(View v) {
-            mViews[0] = v;
-            return this;
-        }
-
-        /**
-         * @param v view, которая находится в центре
-         * @return Builder fluent-interface
-         */
-        public Builder center(View v) {
-            mViews[1] = v;
-            return this;
-        }
-
-        /**
-         * @param v view, которая находится ниже остальных
-         * @return Builder fluent-interface
-         */
-        public Builder down(View v) {
-            mViews[2] = v;
-            return this;
-        }
-
-        /**
-         * Создает параметры однонаправленной анимации для трех view
-         * @return параметры анимации в объекте {@link Params}
-         */
-        public Params compute() {
-            if (mSelected == null || mViews == null) throw new IllegalArgumentException();
-            for (View item : mViews) if (item == null) throw new IllegalArgumentException();
-
-            Params params = new Params();
-            for (View item : mViews) {
-                params.alpha(mSelected == item ? 1 : 0f);
-                if (item == mSelected) { // я выбранный
-                    float distance = Math.abs(mSelected.getY() - mViews[1].getY()) - item.getHeight(); // на сколько
-                    int sign = Float.compare(mViews[1].getY(), mSelected.getY()); //  в какую сторону
-                    float add = (distance * sign); // двигаться
-                    int multi = Float.compare(mViews[1].getY(), item.getY()); // я относительно центра
-                    params.distance(multi, add, item.getHeight());
-
-                } else {
-                    float add = 0f; // дополнительно никуда не двигаться
-                    int multi = Float.compare(item.getY(), mSelected.getY()); // я относительно выбранного
-                    params.distance(multi, add, item.getHeight());
-                }
-            }
-            mSelected = null;
-            mViews = null;
-            if (!params.consistent()) {
-                throw new IllegalStateException();
-            }
-            return params;
-        }
-    }
-
 }
