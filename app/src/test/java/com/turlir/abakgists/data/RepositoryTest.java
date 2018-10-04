be71d48d901bedcd2a4311f8e4b1a2729d724854
turlir <iljaosincev@gmail.com>
2018-10-05 00:07:16 +0500

    remove unused dependencies repository
    - refactoring RepositoryTest

--- app/src/test/java/com/turlir/abakgists/data/RepositoryTest.java
+++ app/src/test/java/com/turlir/abakgists/data/RepositoryTest.java
@@ -1,54 +1,70 @@
-package com.turlir.abakgists.allgists;
+package com.turlir.abakgists.data;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Room;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.os.Build;
 
+import com.google.common.io.Files;
+import com.turlir.abakgists.AppDatabase;
 import com.turlir.abakgists.BuildConfig;
 import com.turlir.abakgists.Data;
-import com.turlir.abakgists.DatabaseMocking;
 import com.turlir.abakgists.api.ApiClient;
 import com.turlir.abakgists.api.Repository;
 import com.turlir.abakgists.api.data.GistJson;
 import com.turlir.abakgists.api.data.GistLocal;
-import com.turlir.abakgists.di.AppComponent;
 
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
 import io.reactivex.Single;
 import io.reactivex.subscribers.TestSubscriber;
-import it.cosenonjaviste.daggermock.DaggerMockRule;
-import it.cosenonjaviste.daggermock.InjectFromComponent;
+
+import static org.junit.Assert.fail;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.O, packageName = "com.turlir.abakgists")
 public class RepositoryTest {
 
-    @Rule
-    public final DaggerMockRule<AppComponent> rule = new DatabaseMocking();
-
-    @InjectFromComponent
-    private Repository _repo;
-
     @Mock
     private ApiClient _client;
 
+    private Repository _repo;
+
+    private AppDatabase database;
+
+    public RepositoryTest() {
+        substitutionDatabase("/test.sql", "test.sqlite");
+        database = Room
+                .databaseBuilder(RuntimeEnvironment.application, AppDatabase.class, "test.sqlite")
+                .allowMainThreadQueries()
+                .build();
+        SupportSQLiteDatabase sqlite = database.getOpenHelper().getReadableDatabase();
+        String path = sqlite.getPath();
+        System.out.println("Path to testing database: " + path);
+    }
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        _repo = new Repository(_client, database.gistDao());
+    }
+
     @Test
     public void roomNotificationAtInsert() {
-        //List<GistLocal> first = partial.blockingFirst();
-        //assertTrue(first.size() > 0);
-
-        /*List<GistLocal> list = mDao.demo(15, 0);
-        int size = list.size();
-        assertTrue(size > 0);*/
-
         TestSubscriber<List<GistLocal>> check = _repo
                 .database(15, 0)
                 .test()
@@ -67,4 +83,16 @@ public class RepositoryTest {
                 .assertValueAt(1, lst -> lst.size() == 2);
     }
 
+    private void substitutionDatabase(final String file, final String basename) {
+        Context cnt = RuntimeEnvironment.application.getApplicationContext();
+        String destinationPath = new ContextWrapper(cnt).getDatabasePath(basename).getAbsolutePath();
+        File to = new File(destinationPath);
+        File from = new File(getClass().getResource(file).getFile());
+        try {
+            Files.copy(from, to);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
 }
